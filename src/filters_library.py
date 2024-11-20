f1942e47bb14b25e33df5a680b0aa1259c8df563
M. Teodoro <mteodoro@stsci.edu>
2024-11-20 16:46:42 -0500

    Add timeout to request.

--- src/filters_library.py
+++ src/filters_library.py
@@ -1,5 +1,6 @@
-import pandas as pd
 from pathlib import Path
+
+import pandas as pd
 import requests
 
 # Base URL for the file download
@@ -8,18 +9,20 @@ BASE_URL = "https://roman.gsfc.nasa.gov/science/RRI/Roman_effarea_{}.xlsx"
 # Default filename with date
 DEFAULT_EFFAREA_FILENAME = "src/data/Roman_effarea_20210614.xlsx"
 
+
 def download_file(url: str, dest: str):
-    response = requests.get(url)
+    response = requests.get(url, timeout=30)
     response.raise_for_status()  # Check if the request was successful
-    with open(dest, 'wb') as file:
+    with open(dest, "wb") as file:
         file.write(response.content)
+
 
 def read_effarea_file(filename: str = None, **kwargs) -> pd.DataFrame:
     if not filename:
         filename = Path(DEFAULT_EFFAREA_FILENAME).resolve()
 
     # Extract the date from the filename
-    date_str = filename.stem.split('_')[-1]
+    date_str = filename.stem.split("_")[-1]
 
     # Construct the URL using the extracted date
     url = BASE_URL.format(date_str)
@@ -30,6 +33,7 @@ def read_effarea_file(filename: str = None, **kwargs) -> pd.DataFrame:
 
     df = pd.read_excel(filename, **kwargs)
     return df
+
 
 if __name__ == "__main__":
     data = read_effarea_file(header=1)
