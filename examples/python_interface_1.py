ffd5539efd5bb47d0ce94b3441f508b31125d5de
Mairan Teodoro <mteodoro@stsci.edu>
2025-03-03 11:21:24 -0500

    Add python interface examples.

--- examples/python_interface_1.py
+++ examples/python_interface_1.py
@@ -1,18 +1,36 @@
-from roman_photoz import roman_catalog_process
-from roman_photoz import default_roman_config
 from pathlib import Path
 
-output_path = Path(".").resolve().as_posix()
+from roman_photoz import default_roman_config, roman_catalog_process
 
+# set the output path to the directory
+# where this script is located
+output_path = Path(__file__).resolve().parent.as_posix()
+
+# we will use the default Roman config
+# and the default input path/filename
+# we will save the results
+# to a file called "output_file_1.asdf"
 argv = [
-    "--config_filename", default_roman_config,
-    "--input_path", "/Users/mteodoro/Library/Caches/lephare/work/",
-    "--input_filename", "roman_simulated_catalog.in",
-    "--output_path", output_path,
-    "--output_filename", "output_file.asdf",
-    "--save_results", "True"
+    "--config_filename",
+    default_roman_config,
+    "--input_path",
+    "/Users/mteodoro/Library/Caches/lephare/work/",
+    "--input_filename",
+    "roman_simulated_catalog.in",
+    "--output_path",
+    output_path,
+    "--output_filename",
+    "output_file_1.asdf",
+    "--save_results",
+    "True",
 ]
 
+####
+# this approach will process the catalog
+# and save the results to a file
+# but will not return anything
+
+# process the catalog
 roman_catalog_process.main(argv)
 
 print("Done!")
--- examples/python_interface_2.py
+++ examples/python_interface_2.py
@@ -1,31 +1,57 @@
-from roman_photoz.roman_catalog_process import RomanCatalogProcess
-from roman_photoz import default_roman_config
 from pathlib import Path
+
 import numpy as np
 from matplotlib import pyplot as plt
 
-output_path = Path("./").resolve().as_posix()
+from roman_photoz import default_roman_config
+from roman_photoz.roman_catalog_process import RomanCatalogProcess
 
+# set the output path to the directory
+# where this script is located
+output_path = Path(__file__).resolve().parent.as_posix()
+
+# we will use the default Roman config
+# and the default input path/filename
+# we will save the results
+# to a file called "output_file_2.asdf"
 argv = [
-    "--config_filename", default_roman_config,
-    "--input_path", "/Users/mteodoro/Library/Caches/lephare/work/",
-    "--input_filename", "roman_simulated_catalog.in",
-    "--output_path", output_path,
-    "--output_filename", "output_file.asdf",
-    "--save_results", "True"
+    "--config_filename",
+    default_roman_config,
+    "--input_path",
+    "/Users/mteodoro/Library/Caches/lephare/work/",
+    "--input_filename",
+    "roman_simulated_catalog.in",
+    "--output_path",
+    output_path,
+    "--output_filename",
+    "output_file_2.asdf",
+    "--save_results",
+    "True",
 ]
 
+####
+# this approach will process the catalog
+# and save the results to a file
+# and will return an instance of RomanCatalogProcess
+# that contains the results
+
+# create a RomanCatalogProcess object
 rcp = RomanCatalogProcess(config_filename=argv[1])
 
+# and process the catalog
 rcp.process(
     input_path=argv[3],
     input_filename=argv[5],
     output_path=argv[7],
     output_filename=argv[9],
-    save_results=argv[11]
+    save_results=argv[11],
 )
 
-zgrid = np.linspace(0, 7, 100)
+# plot the estimated PDF for the first object
+zgrid = np.linspace(0, 7, 200)
 plt.plot(zgrid, np.squeeze(rcp.estimated.data.pdf(zgrid)[0]))
+
+# plot the estimated redshift ("Z_BEST") vs. the actual redshift ("ZSPEC")
+plt.plot(rcp.estimated.data.ancil["ZSPEC"], rcp.estimated.data.ancil["Z_BEST"], "o")
 
 print("Done!")
